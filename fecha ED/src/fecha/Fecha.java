d4bcad4f6aca84056cf7b086501abe98e6932b78
CJ <CJ@DESKTOP-OS2TG00>
2021-02-23 20:56:08 +0100

    8-) Documenta el código que has obtenido como resultado de todas las actividades anteriores de forma que quede claro lo que realiza: qué función realiza cada clase y/o método, cada uno de los parámetros necesarios, el autor, la versión, si se ha quedado algún método obsoleto y cualquier otra cuestión que consideres relevante. Lo realizarás tanto en Eclipse como en Netbeans. Sube las modificaciones al repositorio de Github, siendo el comentario el enunciado de este ejercicio.

--- fecha ED/src/fecha/Fecha.java
+++ fecha ED/src/fecha/Fecha.java
@@ -9,30 +9,36 @@ package fecha;
  */
 public class Fecha {
 
+	//Estos son los datos principales para usar el programa
 	public int dia;
 	public int mes;
 	public int anio;
-
+	
+	//Aqui empezamos a usar el constructor para meter los atributos en un entero
 	public Fecha(int dia, int mes, int anio) {
 		this.dia = dia;
 		this.mes = mes;
 		this.anio = anio;
 	}
 
+	//Aqui nos devolvera el valor de la validacion de fecha
 	public boolean valida() {
 
 		return validarFecha();
 
 	}
 
+	//En este proceso validamos si lo que se esta introduciendo es un valor coherente
 	public boolean validarFecha() {
 		if (dia < 1 || dia > 31)
 			return false;
 		if (mes < 1 || mes > 12)
 			return false;
 
-// Determinamos la cantidad de días del mes:
+		// Determinamos la cantidad de días del mes:
 		int diasMes = 0;
+		
+		//Aqui se ubican los meses que acaban en dia 31 
 		switch (mes) {
 		case 1:
 		case 3:
@@ -43,24 +49,30 @@ public class Fecha {
 		case 12:
 			diasMes = 31;
 			break;
+		
+		//Aqui se ubican los meses que acaban en dia 30 
 		case 4:
 		case 6:
 		case 9:
 		case 11:
 			diasMes = 30;
 			break;
-		case 2: // Verificamos si el año es bisiesto
+			
+		//Aqui nos saldra el valor de la verificacion del proceso "bisiesto()"
+		case 2: 
 			diasMes = bisiesto();
 			break;
 
 		}
-
+		
+		//En este apartado confirmamos si el dia es valido con el mes elegido
 		if (dia > diasMes)
 			return false;
 		else
 			return true;
 	}
 
+	//Y aqui se valida si el mes de Febrero es bisiesto o no
 	public int bisiesto() {
 		int diasMes;
 		if ((anio % 400 == 0) || ((anio % 4 == 0) && (anio % 100 != 0)))
